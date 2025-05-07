1fbc60d5e49cdcf119b9ceba8072c8c8ac1a4eae
Petrov Leonid <petrovleonid1999@mail.ru>
2025-05-07 13:54:16 +0300

    adding new states

--- boiler_telegram_bot/main_menu/boiler_dialog/boiler_dialog_states.py
+++ boiler_telegram_bot/main_menu/boiler_dialog/boiler_dialog_states.py
@@ -3,7 +3,14 @@ from aiogram.fsm.state import StatesGroup, State
 
 class BoilerDialog(StatesGroup):
     boiler_main_menu = State()
+
     boiler_feedback = State()
     boiler_accept_feedback = State()
 
     boiler_repair_menu = State()
+
+    boiler_technical_catalog = State()
+
+    boiler_rent = State()
+
+    boiler_barista_training = State()
