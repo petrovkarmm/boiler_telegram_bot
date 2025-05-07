de7f01fcee5fe4b4c33006bad924c9a181882098
Petrov Leonid <petrovleonid1999@mail.ru>
2025-05-07 19:15:09 +0300

    admin boiler feedbacks states

--- boiler_telegram_bot/main_menu/admin_boiler_dialog/boiler_dialog_states.py
+++ boiler_telegram_bot/main_menu/admin_boiler_dialog/boiler_dialog_states.py
@@ -3,5 +3,8 @@ from aiogram.fsm.state import StatesGroup, State
 
 class AdminBoilerDialog(StatesGroup):
     admin_boiler_main_menu = State()
-    admin_boiler_feedbacks = State()
     admin_boiler_technical_problems = State()
+
+    admin_boiler_feedbacks_menu = State()
+    admin_boiler_new_feedbacks = State()
+    admin_boiler_old_feedbacks = State()
