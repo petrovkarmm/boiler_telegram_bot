2d498162a807cfa1ae7533236fd77cdf84868fe9
Petrov Leonid <petrovleonid1999@mail.ru>
2025-05-07 13:54:29 +0300

    adding feedback sending into db

--- boiler_telegram_bot/main_menu/boiler_dialog/boiler_dialog_on_click_functions.py
+++ boiler_telegram_bot/main_menu/boiler_dialog/boiler_dialog_on_click_functions.py
@@ -7,6 +7,7 @@ from aiogram_dialog.widgets.kbd import Button, ManagedCounter
 from aiogram.exceptions import TelegramRetryAfter
 
 from boiler_telegram_bot.main_menu.boiler_dialog.boiler_dialog_states import BoilerDialog
+from db_configuration.crud import Feedback
 
 
 async def send_feedback(
@@ -14,20 +15,17 @@ async def send_feedback(
 ):
     user_answer = dialog_manager.dialog_data.get('user_answer', 'ERROR')
 
-    user_id = callback.from_user.id
-    user_username = callback.from_user.username
+    tg_user_id = callback.from_user.id
+    user_username = '@' + callback.from_user.username
     user_first_name = callback.from_user.first_name
     user_last_name = callback.from_user.last_name
 
-    await callback.message.answer(
-        text=f'-------------------------\n'
-             f'Передаём в CRM систему фидбек пользователя: {user_answer}\n\n'
-             f'Данные пользователя:\n\n'
-             f'TG ID: {user_id}\n'
-             f'Username: @{user_username}\n'
-             f'Имя: {user_first_name}\n'
-             f'Фамилия: {user_last_name}\n'
-             f'-------------------------'
+    Feedback.add_feedback(
+        tg_user_id=tg_user_id,
+        firstname=user_first_name,
+        lastname=user_last_name,
+        username=user_username,
+        text=user_answer
     )
 
     await callback.message.answer(
