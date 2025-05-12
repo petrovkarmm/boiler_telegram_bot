7810ff5dc631502dd257138d14782bafb372e30a
Petrov Leonid <petrovleonid1999@mail.ru>
2025-05-12 15:13:11 +0300

    technical_problem_view (test)

--- boiler_telegram_bot/main_menu/admin_boiler_dialog/admin_boiler_dialog.py
+++ boiler_telegram_bot/main_menu/admin_boiler_dialog/admin_boiler_dialog.py
@@ -6,14 +6,14 @@ from aiogram_dialog.widgets.kbd import Button, SwitchTo, Back, Group, Row, Scrol
 from aiogram_dialog.widgets.text import Format
 
 from main_menu.admin_boiler_dialog.admin_boiler_dialog_getter import feedbacks_count_getter, feedback_getter, \
-    technical_problem_id_getter, technical_problems_getter
+    technical_problem_id_getter, technical_problems_getter, technical_problem_getter
 from main_menu.admin_boiler_dialog.admin_boiler_dialog_on_click_functions import go_to_boiler_bot, \
     on_feedback_selected, go_to_new_feedbacks, go_to_old_feedbacks, mark_feedback, on_technical_problem_selected
-from main_menu.admin_boiler_dialog.boiler_dialog_states import AdminBoilerDialog
+from main_menu.admin_boiler_dialog.admin_boiler_dialog_states import AdminBoilerDialog
 
-from boiler_telegram_bot.main_menu.admin_boiler_dialog.admin_boiler_dialog_dataclasses import FEEDBACK_KEY, \
-    TECHNICAL_PROBLEM_KEY
-from boiler_telegram_bot.main_menu.admin_boiler_dialog.admin_boiler_dialog_getter import feedbacks_getter, \
+from main_menu.admin_boiler_dialog.admin_boiler_dialog_dataclasses import ADMIN_FEEDBACK_KEY, \
+    ADMIN_TECHNICAL_PROBLEM_KEY
+from main_menu.admin_boiler_dialog.admin_boiler_dialog_getter import feedbacks_getter, \
     feedback_id_getter
 
 admin_boiler_main_menu = Window(
@@ -28,7 +28,7 @@ admin_boiler_main_menu = Window(
         ),
         SwitchTo(
             id='view_problems', text=Format('Редактирование проблем'),
-            state=AdminBoilerDialog.admin_boiler_technical_problems
+            state=AdminBoilerDialog.admin_boiler_technical_problems_list
         ),
     ),
     Button(
@@ -75,7 +75,7 @@ admin_boiler_feedbacks_list = Window(
             Select(
                 text=Format("{item.feedback_text}"),
                 id="feedback_selected",
-                items=FEEDBACK_KEY,
+                items=ADMIN_FEEDBACK_KEY,
                 item_id_getter=feedback_id_getter,
                 on_click=on_feedback_selected,
             ),
@@ -123,6 +123,22 @@ admin_boiler_feedback_view = Window(
     parse_mode=ParseMode.HTML
 )
 
+technical_problem_view = Window(
+    Format(
+        text='Test'
+    ),
+    Row(
+        SwitchTo(
+            id='back_to_new_fb_menu', text=Format('Назад'), state=AdminBoilerDialog.admin_boiler_technical_problems_list
+        ),
+        SwitchTo(
+            id='back_to_menu', text=Format('В меню'), state=AdminBoilerDialog.admin_boiler_main_menu
+        )
+    ),
+    getter=technical_problem_getter,
+    state=AdminBoilerDialog.admin_boiler_technical_problem_view
+)
+
 admin_boiler_technical_problems_list = Window(
     Format(
         text='Выберите проблему для редактирования:'
@@ -132,7 +148,7 @@ admin_boiler_technical_problems_list = Window(
             Select(
                 text=Format("{item.name}"),
                 id="problem_selected",
-                items=TECHNICAL_PROBLEM_KEY,
+                items=ADMIN_TECHNICAL_PROBLEM_KEY,
                 item_id_getter=technical_problem_id_getter,
                 on_click=on_technical_problem_selected,
             ),
@@ -146,7 +162,7 @@ admin_boiler_technical_problems_list = Window(
         id='back_to_menu', text=Format('В меню'), state=AdminBoilerDialog.admin_boiler_main_menu
     ),
     getter=technical_problems_getter,
-    state=AdminBoilerDialog.admin_boiler_technical_problems
+    state=AdminBoilerDialog.admin_boiler_technical_problems_list
 )
 
 admin_boiler_dialog = Dialog(
@@ -156,5 +172,6 @@ admin_boiler_dialog = Dialog(
     admin_boiler_feedbacks_list,
     admin_boiler_feedback_view,
 
-    admin_boiler_technical_problems_list
+    admin_boiler_technical_problems_list,
+    technical_problem_view
 )
