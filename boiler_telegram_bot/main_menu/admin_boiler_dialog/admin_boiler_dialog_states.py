b70e4ded290067c0f7e0d3c192703cf46c054154
Petrov Leonid <petrovleonid1999@mail.ru>
2025-05-12 15:11:36 +0300

    technical_problems list/view states

--- boiler_telegram_bot/main_menu/admin_boiler_dialog/admin_boiler_dialog_states.py
+++ boiler_telegram_bot/main_menu/admin_boiler_dialog/admin_boiler_dialog_states.py
@@ -3,9 +3,10 @@ from aiogram.fsm.state import StatesGroup, State
 
 class AdminBoilerDialog(StatesGroup):
     admin_boiler_main_menu = State()
-    admin_boiler_technical_problems = State()
 
     admin_boiler_feedbacks_menu = State()
     admin_boiler_feedbacks_list = State()
-
     admin_boiler_feedback_view = State()
+
+    admin_boiler_technical_problems_list = State()
+    admin_boiler_technical_problem_view = State()
