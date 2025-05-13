e9826a4ed2b09878c2556838292dd17996c9e478
Petrov Leonid <petrovleonid1999@mail.ru>
2025-05-13 16:47:04 +0300

    with conn

--- boiler_telegram_bot/db_configuration/models/feedback.py
+++ boiler_telegram_bot/db_configuration/models/feedback.py
@@ -6,59 +6,52 @@ from db_configuration.db import get_connection
 class Feedback:
     @staticmethod
     def add_feedback(tg_user_id: int, firstname: str, lastname: str, username: str, text: str):
-        conn = get_connection()
-        cursor = conn.cursor()
+        with get_connection() as conn:
+            cursor = conn.cursor()
 
-        cursor.execute("""
-        INSERT INTO feedback (tg_user_id, user_firstname, user_lastname, user_username, feedback_text)
-        VALUES (?, ?, ?, ?, ?)
-        """, (tg_user_id, firstname, lastname, username, text))
-
-        conn.commit()
-        conn.close()
+            cursor.execute("""
+            INSERT INTO feedback (tg_user_id, user_firstname, user_lastname, user_username, feedback_text)
+            VALUES (?, ?, ?, ?, ?)
+            """, (tg_user_id, firstname, lastname, username, text))
 
     @staticmethod
     def get_all_unviewed_feedback():
-        conn = get_connection()
-        conn.row_factory = sqlite3.Row
-        cursor = conn.cursor()
+        with get_connection() as conn:
+            conn.row_factory = sqlite3.Row
+            cursor = conn.cursor()
 
-        cursor.execute("SELECT * FROM feedback WHERE viewed = 0")
-        results = cursor.fetchall()
+            cursor.execute("SELECT * FROM feedback WHERE viewed = 0")
+            results = cursor.fetchall()
 
-        conn.close()
-        return results
+            return results
 
     @staticmethod
     def get_all_viewed_feedback():
-        conn = get_connection()
-        conn.row_factory = sqlite3.Row
-        cursor = conn.cursor()
+        with get_connection() as conn:
+            conn.row_factory = sqlite3.Row
+            cursor = conn.cursor()
 
-        cursor.execute("SELECT * FROM feedback WHERE viewed = 1")
-        results = cursor.fetchall()
+            cursor.execute("SELECT * FROM feedback WHERE viewed = 1")
+            results = cursor.fetchall()
 
-        conn.close()
-        return results
+            return results
 
     @staticmethod
     def mark_feedback_viewed(feedback_id: int):
-        conn = get_connection()
-        cursor = conn.cursor()
+        with get_connection() as conn:
+            cursor = conn.cursor()
 
-        cursor.execute("UPDATE feedback SET viewed = 1 WHERE id = ?", (feedback_id,))
-        conn.commit()
-        conn.close()
+            cursor.execute("UPDATE feedback SET viewed = 1 WHERE id = ?", (feedback_id,))
+            conn.commit()
 
     @staticmethod
     def get_feedback_by_id(feedback_id: int):
-        conn = get_connection()
-        conn.row_factory = sqlite3.Row
-        cursor = conn.cursor()
+        with get_connection() as conn:
+            conn.row_factory = sqlite3.Row
+            cursor = conn.cursor()
 
-        cursor.execute("SELECT * FROM feedback WHERE id = ?",
-                       (feedback_id,))
-        result = cursor.fetchone()
+            cursor.execute("SELECT * FROM feedback WHERE id = ?",
+                           (feedback_id,))
+            result = cursor.fetchone()
 
-        conn.close()
-        return result
+            return result
