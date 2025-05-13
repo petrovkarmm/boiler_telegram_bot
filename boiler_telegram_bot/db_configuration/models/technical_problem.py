527abce75307959bbe569e42cb3bca8ba12bee10
Petrov Leonid <petrovleonid1999@mail.ru>
2025-05-13 16:47:28 +0300

    with con

--- boiler_telegram_bot/db_configuration/models/technical_problem.py
+++ boiler_telegram_bot/db_configuration/models/technical_problem.py
@@ -6,73 +6,67 @@ from db_configuration.db import get_connection
 class TechnicalProblem:
     @staticmethod
     def get_all_technical_problem():
-        conn = get_connection()
-        conn.row_factory = sqlite3.Row
-        cursor = conn.cursor()
+        with get_connection() as conn:
+            conn.row_factory = sqlite3.Row
+            cursor = conn.cursor()
 
-        cursor.execute("SELECT * FROM technical_problem")
-        results = cursor.fetchall()
+            cursor.execute("SELECT * FROM technical_problem")
+            results = cursor.fetchall()
 
-        conn.close()
-        return results
+            return results
 
     @staticmethod
     def get_all_unhidden_technical_problem():
-        conn = get_connection()
-        conn.row_factory = sqlite3.Row
-        cursor = conn.cursor()
+        with get_connection() as conn:
+            conn.row_factory = sqlite3.Row
+            cursor = conn.cursor()
 
-        cursor.execute("SELECT * FROM technical_problem WHERE hidden = 0")
-        results = cursor.fetchall()
+            cursor.execute("SELECT * FROM technical_problem WHERE hidden = 0")
+            results = cursor.fetchall()
 
-        conn.close()
-        return results
+            return results
 
     @staticmethod
     def get_technical_problem_by_id(technical_problem_id: int):
-        conn = get_connection()
-        conn.row_factory = sqlite3.Row
-        cursor = conn.cursor()
+        with get_connection() as conn:
+            conn.row_factory = sqlite3.Row
+            cursor = conn.cursor()
 
-        cursor.execute("SELECT * FROM technical_problem WHERE id = ?", (technical_problem_id,))
-        result = cursor.fetchone()
+            cursor.execute("SELECT * FROM technical_problem WHERE id = ?", (technical_problem_id,))
+            result = cursor.fetchone()
 
-        conn.close()
-        return result
+            return result
 
     @staticmethod
     def toggle_hidden_by_id(technical_problem_id: int):
-        conn = get_connection()
-        cursor = conn.cursor()
+        with get_connection() as conn:
+            cursor = conn.cursor()
 
-        cursor.execute("""
-            UPDATE technical_problem
-            SET hidden = NOT hidden,
-                updated = CURRENT_TIMESTAMP
-            WHERE id = ?
-        """, (technical_problem_id,))
+            cursor.execute("""
+                UPDATE technical_problem
+                SET hidden = NOT hidden,
+                    updated = CURRENT_TIMESTAMP
+                WHERE id = ?
+            """, (technical_problem_id,))
 
-        conn.commit()
-        conn.close()
+            conn.commit()
 
     @staticmethod
     def delete_technical_problem_by_id(technical_problem_id: int):
-        conn = get_connection()
-        cursor = conn.cursor()
+        with get_connection() as conn:
+            cursor = conn.cursor()
 
-        cursor.execute("DELETE FROM technical_problem WHERE id = ?", (technical_problem_id,))
-        conn.commit()
-        conn.close()
+            cursor.execute("DELETE FROM technical_problem WHERE id = ?", (technical_problem_id,))
+            conn.commit()
 
     @staticmethod
     def add_technical_problem(name: str, hidden: int):
-        conn = get_connection()
-        cursor = conn.cursor()
+        with get_connection() as conn:
+            cursor = conn.cursor()
 
-        cursor.execute("""
-        INSERT INTO technical_problem (name, hidden)
-        VALUES (?, ?)
-        """, (name, hidden))
+            cursor.execute("""
+            INSERT INTO technical_problem (name, hidden)
+            VALUES (?, ?)
+            """, (name, hidden))
 
-        conn.commit()
-        conn.close()
+            conn.commit()
--- boiler_telegram_bot/db_configuration/models/user.py
+++ boiler_telegram_bot/db_configuration/models/user.py
@@ -5,15 +5,61 @@ from db_configuration.db import get_connection
 
 class User:
     @staticmethod
-    def add_user_if_not_exists(telegram_id: int, name: str):
-        conn = get_connection()
-        cursor = conn.cursor()
+    def check_user_in_database(telegram_id: int):
+        with get_connection() as conn:
+            cursor = conn.cursor()
 
-        cursor.execute("SELECT id FROM users WHERE telegram_id = ?", (telegram_id,))
-        user = cursor.fetchone()
+            cursor.execute("SELECT id FROM user WHERE telegram_id = ?", (telegram_id,))
+            user = cursor.fetchone()
 
-        if user is None:
-            cursor.execute("INSERT INTO users (telegram_id, name) VALUES (?, ?)", (telegram_id, name))
+            if user is None:
+                return False
+
+            return True
+
+    @staticmethod
+    def add_user(
+            telegram_id: str,
+            telegram_first_name: str = None,
+            telegram_last_name: str = None,
+            telegram_username: str = None,
+            name: str = None,
+            organization_itn: str = None,
+            organization_name: str = None,
+            phone: str = None
+    ):
+        with get_connection() as conn:
+            cursor = conn.cursor()
+
+            cursor.execute("""
+                INSERT INTO user (
+                    telegram_id,
+                    telegram_first_name,
+                    telegram_last_name,
+                    telegram_username,
+                    name,
+                    organization_itn,
+                    organization_name,
+                    phone
+                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+            """, (
+                telegram_id,
+                telegram_first_name,
+                telegram_last_name,
+                telegram_username,
+                name,
+                organization_itn,
+                organization_name,
+                phone
+            ))
+
             conn.commit()
 
-        conn.close()
+    @staticmethod
+    def get_user_by_telegram_id(telegram_id: int) -> dict | None:
+        with get_connection() as conn:
+            conn.row_factory = sqlite3.Row  # Позволяет получать строки как словари
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM user WHERE telegram_id = ?", (telegram_id,))
+            row = cursor.fetchone()
+            return dict(row) if row else None
