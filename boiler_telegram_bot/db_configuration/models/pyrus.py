51405b1d1089afa1e6b6281302b87d1e856a66f0
Petrov Leonid <petrovleonid1999@mail.ru>
2025-05-23 16:34:39 +0300

    fix update token

--- boiler_telegram_bot/db_configuration/models/pyrus.py
+++ boiler_telegram_bot/db_configuration/models/pyrus.py
@@ -1,4 +1,5 @@
 from db_configuration.db import get_connection
+from tg_logs.logger import bot_logger
 
 
 #  TODO rename
@@ -20,12 +21,12 @@ class PyrusToken:
             return (row[0], row[1]) if row else None
 
     @staticmethod
-    def update_token(old_token: str, new_token: str):
+    def update_token(pyrus_login: str, new_token: str):
         with get_connection() as conn:
             cursor = conn.cursor()
             cursor.execute(
-                "UPDATE pyrus_token SET access_token = ?, updated = CURRENT_TIMESTAMP WHERE access_token = ?",
-                (new_token, old_token)
+                "UPDATE pyrus_token SET access_token = ?, updated = CURRENT_TIMESTAMP WHERE pyrus_login = ?",
+                (new_token, pyrus_login)
             )
 
     @staticmethod
@@ -40,7 +41,7 @@ class PyrusToken:
             existing = cursor.fetchone()
 
             if existing:
-                print("Запись с таким security_key уже существует. Пропускаем вставку.")
+                bot_logger.warning("Запись с таким security_key уже существует. Пропускаем вставку.")
                 return
 
             cursor.execute("""
