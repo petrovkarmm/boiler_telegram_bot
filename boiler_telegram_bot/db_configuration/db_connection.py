1eda00aa684b55fe37be3cd505405401ce27b68f
dkarmm <petrovleonid1999@mail.ru>
2025-05-25 19:29:15 +0300

    adding path for connection

--- boiler_telegram_bot/db_configuration/db_connection.py
+++ boiler_telegram_bot/db_configuration/db_connection.py
@@ -1,8 +1,9 @@
 import os
 import sqlite3
 
-DB_PATH = os.path.join(os.path.dirname(__file__), 'bot.db')
-
+DB_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'db')
+DB_PATH = os.path.join(DB_DIR, 'bot.db')
 
 def get_connection():
+    os.makedirs(DB_DIR, exist_ok=True)
     return sqlite3.connect(DB_PATH)
