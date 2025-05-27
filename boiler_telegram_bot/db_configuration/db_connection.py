fc67c6f31d295179f0367ebf1220ff87637ba6c9
Petrov Leonid <petrovleonid1999@mail.ru>
2025-05-27 16:04:58 +0300

    pep8

--- boiler_telegram_bot/db_configuration/db_connection.py
+++ boiler_telegram_bot/db_configuration/db_connection.py
@@ -4,6 +4,7 @@ import sqlite3
 DB_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'db')
 DB_PATH = os.path.join(DB_DIR, 'bot.db')
 
+
 def get_connection():
     os.makedirs(DB_DIR, exist_ok=True)
     return sqlite3.connect(DB_PATH)
