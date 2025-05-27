9d83d4d766a55b74e9dc75716b03ce63b2a3e8ad
Petrov Leonid <petrovleonid1999@mail.ru>
2025-05-27 16:05:01 +0300

    pep8

--- boiler_telegram_bot/db_configuration/insert_values_in_db.py
+++ boiler_telegram_bot/db_configuration/insert_values_in_db.py
@@ -5,5 +5,6 @@ from boiler_telegram_bot.settings import pyrus_login, pyrus_security_key
 def insert_values():
     PyrusToken.insert_login_data(pyrus_login=pyrus_login, pyrus_security_key=pyrus_security_key)
 
+
 if __name__ == '__main__':
     insert_values()
