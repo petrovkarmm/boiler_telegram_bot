16799ea4debdf37290401960cc69149b2c3befbb
Petrov Leonid <petrovleonid1999@mail.ru>
2025-05-27 16:05:23 +0300

    adding firm type

--- boiler_telegram_bot/main_menu/boiler_registration_dialog/boiler_registration_states.py
+++ boiler_telegram_bot/main_menu/boiler_registration_dialog/boiler_registration_states.py
@@ -5,6 +5,9 @@ class BoilerRegistrationDialog(StatesGroup):
     boiler_registration_user_name = State()
     boiler_registration_itn = State()
     boiler_registration_phone = State()
+
+    boiler_registration_firm_type = State()
+
     boiler_registration_organization_name = State()
 
     boiler_registration_accepting = State()
