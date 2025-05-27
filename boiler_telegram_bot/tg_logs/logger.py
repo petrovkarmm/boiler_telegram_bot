1f31d2c8471888a1759f29849a1f39f31747848d
petrovleonid <petrovleonid1999@mail.ru>
2025-05-27 21:08:03 +0300

    deleting trash

--- boiler_telegram_bot/tg_logs/logger.py
+++ boiler_telegram_bot/tg_logs/logger.py
@@ -34,31 +34,3 @@ async def handler_log(user, state, start_data, dialog_data):
         f"| Состояние: {state} | start_data: {start_data} | dialog_data: {dialog_data} | User: {user} |"
     )
     bot_logger.info(log_message)
-
-
-def log_async_request(func):
-    @wraps(func)
-    async def wrapper(*args, **kwargs):
-        log_message = f"Вызов запроса: {func.__name__} | Аргументы: {args} | Каргументы: {kwargs}"
-        bot_logger.info(log_message)
-
-        start_time = time.time()
-        try:
-            result = await func(*args, **kwargs)
-
-            if isinstance(result, tuple) and len(result) == 2:
-                status_code, response = result
-                bot_logger.info(f"Окончание запроса: {func.__name__} | Статус код: {status_code} | "
-                                f"Ответ: {response}")
-            else:
-                bot_logger.info(f"Окончание запроса: {func.__name__} | Ответ: {result}")
-
-            execution_time = time.time() - start_time
-            bot_logger.info(f"Время ожидания: {execution_time:.4f} секунд")
-
-            return result
-        except Exception as e:
-            bot_logger.error(f"Ошибка в запросе: {func.__name__}\n{e}", exc_info=True)
-            raise e
-
-    return wrapper
