39e13d458a99a9fcae8d2cd9958d6ab1c5bcaa53
Petrov Leonid <petrovleonid1999@mail.ru>
2025-05-27 16:05:12 +0300

    fix logger paths

--- boiler_telegram_bot/settings.py
+++ boiler_telegram_bot/settings.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-DEBUG = False
+DEBUG = True
 
 redis_connect_url = os.getenv("REDIS_CONNECT_URL")
 bot_token = os.getenv("BOT_TOKEN")
@@ -14,4 +14,4 @@ pyrus_security_key = os.getenv('PYRUS_SECURITY_KEY')
 pyrus_standard_url = 'https://api.pyrus.com/v4'
 admin_panel_password = os.getenv("ADMIN_PANEL_PASSWORD")
 
-BOT_BASE_DIR = os.getcwd()
+BOT_BASE_DIR = os.path.dirname(os.path.abspath(__file__))
