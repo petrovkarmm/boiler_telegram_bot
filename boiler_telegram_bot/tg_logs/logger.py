226aac83453fab83415068f38df68e531b0f2758
Petrov Leonid <petrovleonid1999@mail.ru>
2025-05-27 16:05:08 +0300

    fix logger paths

--- boiler_telegram_bot/tg_logs/logger.py
+++ boiler_telegram_bot/tg_logs/logger.py
@@ -1,22 +1,29 @@
 import logging
-import time
-from functools import wraps
+import os
 from logging.handlers import TimedRotatingFileHandler
 
 from boiler_telegram_bot.settings import BOT_BASE_DIR
 
+log_dir = os.path.join(BOT_BASE_DIR, 'tg_logs')
+os.makedirs(log_dir, exist_ok=True)
+
+log_file_path = os.path.join(log_dir, 'aiogram.log')
+
 bot_logger = logging.getLogger('bot_logger')
 bot_logger.setLevel(logging.INFO)
+
 bot_file_handler = TimedRotatingFileHandler(
-    f'{BOT_BASE_DIR}/tg_logs/aiogram.log',
+    filename=log_file_path,
     when='W0',
     interval=1,
     backupCount=4,
     encoding='utf-8'
 )
 bot_file_handler.setLevel(logging.INFO)
+
 bot_formatter = logging.Formatter('%(asctime)s - [%(levelname)s] - %(message)s')
 bot_file_handler.setFormatter(bot_formatter)
+
 bot_logger.addHandler(bot_file_handler)
 
 logging.getLogger('aiogram').setLevel(logging.WARNING)
@@ -24,7 +31,7 @@ logging.getLogger('aiogram').setLevel(logging.WARNING)
 
 async def handler_log(user, state, start_data, dialog_data):
     log_message = (
-        f"Состояние: {state} | start_data: {start_data} | dialog_data: {dialog_data}User: {user} |"
+        f"| Состояние: {state} | start_data: {start_data} | dialog_data: {dialog_data} | User: {user} |"
     )
     bot_logger.info(log_message)
 
