1979523397b0210049800410f19c8a7e0e6c98c0
dkarmm <petrovleonid1999@mail.ru>
2025-05-28 17:25:02 +0300

    profiles getter + deleting old getter from rent

--- boiler_telegram_bot/main_menu/boiler_dialog/boiler_dialog_getter.py
+++ boiler_telegram_bot/main_menu/boiler_dialog/boiler_dialog_getter.py
@@ -3,43 +3,36 @@ from aiogram_dialog.widgets.kbd import ManagedRadio
 
 from boiler_telegram_bot.db_configuration.models.technical_problem import TechnicalProblem
 from boiler_telegram_bot.db_configuration.models.user import User
-from boiler_telegram_bot.main_menu.boiler_dialog.boiler_dialog_dataclasses import TechnicalProblemDialog, TECHNICAL_PROBLEM_KEY, \
-    TECHNICAL_CATALOG, RENT_TYPE
+from boiler_telegram_bot.main_menu.boiler_dialog.boiler_dialog_dataclasses import TechnicalProblemDialog, \
+    TECHNICAL_PROBLEM_KEY, \
+    TECHNICAL_CATALOG, RENT_TYPE, ProfileDialog, PROFILE_KEY
 from boiler_telegram_bot.tg_logs.logger import bot_logger
+from boiler_telegram_bot.db_configuration.models.firm import Firm
 
 
 def technical_problem_id_getter(technical_problem: TechnicalProblemDialog) -> int:
     return technical_problem.id
 
 
-async def user_data_profile_barista_getter(dialog_manager: DialogManager, **_kwargs):
+def profile_id_getter(profile: ProfileDialog) -> int:
+    return profile.id
+
+
+async def profiles_getter(dialog_manager: DialogManager, **_kwargs):
     event_update = _kwargs.get('event_update')
 
     user_id = str(event_update.event.from_user.id)
 
-    user_data = User.get_user_by_telegram_id(
-        user_id
-    )
+    user_firms = Firm.get_firms_by_telegram_id(user_id)
 
-    if user_data:
-        barista_value = dialog_manager.dialog_data['barista_value']
-        user_phone = user_data['phone']
-        user_name = user_data['name']
-        organization_itn = user_data['organization_itn']
-        organization_name = user_data['organization_name']
-
-        return {
-            'user_phone': user_phone,
-            'user_name': user_name,
-            'organization_name': organization_name,
-            'organization_itn': organization_itn,
-            'barista_value': barista_value
-        }
-    else:
-        bot_logger.warning(
-            f'Незарегистрированный пользователь в меню. | {user_id}'
-        )
-        return None
+    return {
+        PROFILE_KEY: [
+            ProfileDialog(
+                firm['firm_id'], ProfileDialog.formatted_name(name=firm['name'], firm_type=firm['firm_type'])
+            )
+            for firm in user_firms
+        ]
+    }
 
 
 async def technical_catalog_getter(**kwargs):
@@ -65,51 +58,6 @@ async def video_or_photo_format_data(dialog_manager: DialogManager, **_kwargs):
     return {
 
     }
-
-
-async def rent_data_for_accept_request(dialog_manager: DialogManager, **_kwargs):
-    event_update = _kwargs.get('event_update')
-
-    user_id = str(event_update.event.from_user.id)
-
-    user_data = User.get_user_by_telegram_id(
-        user_id
-    )
-
-    if user_data:
-        rent_radio_rent_type_widget = dialog_manager.find(
-            'rent_type'
-        )
-        rent_radio_catalog_widget = dialog_manager.find(
-            'rent_tech_type'
-        )
-
-        rent_radio_rent_type_widget: ManagedRadio
-        rent_radio_catalog_widget: ManagedRadio
-
-        user_rent_type = RENT_TYPE.get(rent_radio_rent_type_widget.get_checked(), 'ERROR')
-        user_technical_type = TECHNICAL_CATALOG.get(rent_radio_catalog_widget.get_checked(), 'ERROR')
-
-        user_phone = user_data['phone']
-        user_name = user_data['name']
-        organization_itn = user_data['organization_itn']
-        organization_name = user_data['organization_name']
-
-        return {
-            "user_rent_type": user_rent_type,
-            "user_phone": user_phone,
-            "user_name": user_name,
-            "organization_itn": organization_itn,
-            "organization_name": organization_name,
-            "user_technical_type": user_technical_type
-        }
-
-
-    else:
-        bot_logger.warning(
-            f'Незарегистрированный пользователь в меню. | {user_id}'
-        )
-        return None
 
 
 async def get_technical_catalog_data_for_accept(dialog_manager: DialogManager, **_kwargs):
