a949026ae02d914ccc08e9dbb8885fe4fd3494b2
dkarmm <petrovleonid1999@mail.ru>
2025-05-28 17:22:49 +0300

    new firm methods

--- boiler_telegram_bot/db_configuration/models/firm.py
+++ boiler_telegram_bot/db_configuration/models/firm.py
@@ -10,8 +10,97 @@ class Firm:
             # Добавляем новую фирму
             cursor.execute("""
                 INSERT INTO firm (user_id, firm_type)
-                VALUES (?, ?, ?)
+                VALUES (?, ?)
             """, (user_id, firm_type))
 
             conn.commit()
             return cursor.lastrowid
+
+    @staticmethod
+    def get_firms_by_telegram_id(telegram_id: str) -> list[dict]:
+        with get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute("""
+                SELECT
+                    f.id AS firm_id,
+                    f.firm_type,
+                    COALESCE(le.organization_name, ind.name) AS name
+                FROM firm f
+                JOIN user u ON f.user_id = u.id
+                LEFT JOIN firm_info_legal_entity le ON le.firm_id = f.id
+                LEFT JOIN firm_info_individual ind ON ind.firm_id = f.id
+                WHERE u.telegram_id = ?
+            """, (telegram_id,))
+
+            rows = cursor.fetchall()
+
+            return [
+                {
+                    "firm_id": row[0],
+                    "firm_type": row[1],
+                    "name": row[2]
+                }
+                for row in rows
+            ]
+
+    @staticmethod
+    def get_firm_info_by_id(firm_id: int) -> dict | None:
+        with get_connection() as conn:
+            cursor = conn.cursor()
+
+            cursor.execute("SELECT firm_type FROM firm WHERE id = ?", (firm_id,))
+            result = cursor.fetchone()
+            if not result:
+                return None
+
+            firm_type = result[0]
+
+            if firm_type == 'legal_entity':
+                cursor.execute("""
+                    SELECT
+                        f.id AS firm_id,
+                        f.user_id,
+                        'legal_entity' AS firm_type,
+                        le.organization_name,
+                        le.organization_representative_name,
+                        le.organization_itn,
+                        le.phone
+                    FROM firm f
+                    JOIN firm_info_legal_entity le ON le.firm_id = f.id
+                    WHERE f.id = ?
+                """, (firm_id,))
+                row = cursor.fetchone()
+                if row:
+                    return {
+                        "firm_id": row[0],
+                        "user_id": row[1],
+                        "firm_type": row[2],
+                        "organization_name": row[3],
+                        "representative_name": row[4],
+                        "itn": row[5],
+                        "phone": row[6],
+                    }
+
+            elif firm_type == 'individual':
+                cursor.execute("""
+                    SELECT
+                        f.id AS firm_id,
+                        f.user_id,
+                        'individual' AS firm_type,
+                        ind.name,
+                        ind.phone
+                    FROM firm f
+                    JOIN firm_info_individual ind ON ind.firm_id = f.id
+                    WHERE f.id = ?
+                """, (firm_id,))
+                row = cursor.fetchone()
+                if row:
+                    return {
+                        "firm_id": row[0],
+                        "user_id": row[1],
+                        "firm_type": row[2],
+                        "name": row[3],
+                        "phone": row[4],
+                    }
+
+            return None
